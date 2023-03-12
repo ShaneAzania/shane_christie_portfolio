98e001cff1c8892ab72879813e45fe91ab184f3e
ShaneAzania <87488883+ShaneAzania@users.noreply.github.com>
2023-03-12 14:32:39 -0400

    minor fix

--- src/components/project-box/project-box.jsx
+++ src/components/project-box/project-box.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import './project-box.styles.scss'
+import { Link } from 'react-router-dom';
+import './project-box.styles.scss';
 
 const ProjectBox = ({ projects }) => {
 	return (
 		<div className='projects-box'>
 			{projects.map(({ name, img, link }) => (
-				<div key={name} className='project' style={{ background: `none`, border: 'red solid 1px' }}>
+				<Link key={name} className='project' style={{}}>
 					<img src='' alt={`${name} project`} />
 					<h3 className='name'>{name}</h3>
-				</div>
+				</Link>
 			))}
 		</div>
 	);
