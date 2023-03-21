641bffc008d71fe5a63ef987bf5e4321070e56bc
ShaneAzania <87488883+ShaneAzania@users.noreply.github.com>
2023-03-21 16:29:24 -0400

    portfolio section done

--- src/components/project-box/project-box.jsx
+++ src/components/project-box/project-box.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Fragment } from 'react';
 import './project-box.styles.scss';
 
 const ProjectBox = ({ projects }) => {
 	return (
-		<div className='projects-box'>
-			{projects.map(({ name, img, link }) => (
-				<Link key={name} className='project' style={{}}>
-					<img src='' alt={`${name} project`} />
-					<h3 className='name'>{name}</h3>
-				</Link>
-			))}
-		</div>
+		<Fragment>
+			<div className='projects-box'>
+				{projects.map(({ name, img, link }) => (
+					<a
+						key={'portfolio' + name}
+						className='project'
+						href={link}
+						target='_blank'
+						rel='noreferrer'
+					>
+						<img src={img} alt={`${name} project`} />
+						<h3 className='name'>{name}</h3>
+					</a>
+				))}
+			</div>
+		</Fragment>
 	);
 };
 
--- src/components/sections/portfolio/portfolio.jsx
+++ src/components/sections/portfolio/portfolio.jsx
@@ -4,23 +4,20 @@ import './portfolio.styles.scss';
 // components
 import ProjectBox from '../../project-box/project-box';
 
-const projects = [
-	{
-		name: 'project 1',
-		img: '',
-		link: '',
-	},
-	{
-		name: 'project 2',
-		img: '',
-		link: '',
-	},
-	{
-		name: 'project 3',
-		img: '',
-		link: '',
-	},
-];
+const dev_projects = [
+		{
+			name: 'Seviin Threads',
+			img: '/assets/imgs/portfolio/dev/ScreenShot2023-03-18.png',
+			link: 'https://dapper-melba-87d671.netlify.app/',
+		},
+	],
+	visual_projects = [
+		{
+			name: 'Visual Art Portfolio',
+			img: '/assets/imgs/portfolio/6C9634AA-FB43-46CE-9401-4A231B472885_1_105_c.jpeg',
+			link: 'https://ShaneAzania.com/',
+		},
+	];
 
 const Portfolio = () => {
 	return (
@@ -31,15 +28,16 @@ const Portfolio = () => {
 					<h2>Websites and Web Applications</h2>
 				</div>
 
-				<ProjectBox projects={projects} />
+				<ProjectBox projects={dev_projects} />
+				<p className='scroll-message'>Scroll→</p>
 			</div>
 
 			<div className='row'>
 				<div className='title-box'>
 					<h1>Portfolio</h1>
-					<h2>Visual Work</h2>
+					<h2>Photo + Video</h2>
 				</div>
-				<ProjectBox projects={projects} />
+				<ProjectBox projects={visual_projects} />
 			</div>
 		</section>
 	);
